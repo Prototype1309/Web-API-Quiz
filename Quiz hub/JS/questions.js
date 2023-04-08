5c9ebb5fb0c958e1f9c98ada36728a91937141f2
prototype1309 <simo.hourri@gmail.com>
2023-04-08 16:34:57 -0400

    addind CSS file and adding questions to JS

---
+++ Quiz hub/JS/questions.js
@@ -0,0 +1,62 @@
+// creatting an array and passing the number, questions, options and answers //
+var questions = [ 
+    { 
+    numb: 1,
+    question:"What does HTML stand for?",
+    answer:"Hyper Text Markup Language",
+    options: [ 
+        "Hyper Text Preprocessor",
+        "Hyper Text Markup Language",
+        "Hyper Text Multiple Language",
+        "Hyper Tool Multi Language",
+    ]
+    },
+
+    { 
+        numb: 2,
+        question:"What does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        options: [ 
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language",
+        ]
+    },
+
+    { 
+        numb: 3,
+        question:"What does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        options: [ 
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language",
+        ]
+    },
+
+    { 
+        numb: 4,
+        question:"What does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        options: [ 
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language",
+        ]
+    },
+
+    { 
+        numb: 5,
+        question:"What does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        options: [ 
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language",
+        ]
+    },
+]
